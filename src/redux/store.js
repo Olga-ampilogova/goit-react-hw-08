9521272d1eb223f5acbbb2b0faec50409727782c
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-04-11 19:27:44 +0200

    Fixing the struture of App

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
- import contactsReducer from '../contacts/slice';
-import filtersReducer from '../filters/slice';
-import authReducer from "../auth/slice";
+ import contactsReducer from './contacts/slice';
+import filtersReducer from './filters/slice';
+import authReducer from "./auth/slice";
 import storage from 'redux-persist/lib/storage';
 import {
   persistStore,
